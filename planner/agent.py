06035a0f3484780606b0c478c888e1f23d9c0e5b
Isaac Blanco <nuclearhands2001@gmail.com>
2026-05-13 19:08:27 -0500

    adding models and working agents

--- planner/agent.py
+++ planner/agent.py
@@ -1,7 +1,12 @@
 from google.adk.agents.llm_agent import Agent
+from google.adk.models.lite_llm import LiteLlm
+
+model = LiteLlm(
+    model='openai/gpt-4o',
+)
 
 root_agent = Agent(
-    model='<FILL_IN_MODEL>',
+    model=model,
     name='root_agent',
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge',
