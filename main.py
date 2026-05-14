bd34014a226e5473331bc687388010a0ff565d86
Isaac Blanco <nuclearhands2001@gmail.com>
2026-05-13 23:29:52 -0500

    simplify the actions to have less issues with json logic

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from a2a-agentes!")
-
-
-if __name__ == "__main__":
-    main()
--- server/actions.py
+++ server/actions.py
@@ -1,27 +1,44 @@
-"""
-This module contains the nodes for the workflow, in 
-Temporal terms, we can say that this are the Actions"
-"""
-
 from temporalio import activity
-from models import Clients
-from sqlmodel import select
-from pydantic import BaseModel
+from sqlmodel import select, Session
 from typing import Any
-from sqlmodel import Session
-from models import engine
 
-class GenericOutput(BaseModel):
-    data: dict[str, Any]
+from models import Clients, Projects, engine
 
 @activity.defn(name="get_clients")
-async def get_clients(input: dict[str, Any]) -> GenericOutput:
+async def get_clients(input: dict[str, Any]) -> dict[str, list[Any]]:
+
     with Session(engine) as session:
+
         statement = select(Clients).limit(10)
-        result = session.exec(statement)
-        clients = result.all()
-        return GenericOutput(data={"clients": clients})
-        
+
+        clients = session.exec(statement).all()
+
+        return {
+            "items": [
+                client.model_dump()
+                for client in clients
+            ]
+        }
+
+
+@activity.defn(name="get_projects")
+async def get_projects(input: dict[str, Any]) -> dict[str, list[Any]]:
+
+    with Session(engine) as session:
+
+        statement = select(Projects).limit(10)
+
+        projects = session.exec(statement).all()
+
+        return {
+            "items": [
+                project.model_dump()
+                for project in projects
+            ]
+        }
+
+
 REGISTRY = {
-    "get_clients": get_clients
+    "get_clients": get_clients,
+    "get_projects": get_projects,
 }
