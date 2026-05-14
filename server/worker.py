c72848b4da04635ed9a89a690f98bff40b63e1f1
Isaac Blanco <nuclearhands2001@gmail.com>
2026-05-13 21:32:09 -0500

    add: worker def

---
+++ server/worker.py
@@ -0,0 +1,20 @@
+import asyncio
+from temporalio.client import Client
+from temporalio.worker import Worker
+from workflow import DynamicWorkflow
+from actions import get_clients
+
+async def main():
+    client = await Client.connect("localhost:7233")
+
+    worker = Worker(
+        client,
+        task_queue="workflow",
+        workflows=[DynamicWorkflow],
+        activities=[get_clients],
+    )
+
+    await worker.run()
+
+if __name__ == "__main__":
+    asyncio.run(main())
